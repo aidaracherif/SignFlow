cd1f0a8099aa1df770b371d18cd8ed4875581197
Mame Diarra <aidaradiarracherif29@gmail.com>
2025-05-20 16:33:46 +0000

    Restriction creation utilisateur, authentification & changement de mot de passe

--- backend/src/routes/utilisateurRoutes.js
+++ backend/src/routes/utilisateurRoutes.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const { createUtilisateur } = require('../controllers/utilisateurController');
+const { createUtilisateur, login, resetPassword } = require('../controllers/utilisateurController');
+const { isAuthenticated } = require('../middleware/auth');
+
+function isAdmin(req, res, next) {
+    if (req.user && req.user.role === 'ADMIN') {
+        next();
+    }
+    else {
+        res.status(403).json({ message: 'Accès refusé. Vous devez être un administrateur.' });
+    }
+}
+router.post('/login', login);
+router.post('/utilisateurs', isAuthenticated, isAdmin, createUtilisateur);
+router.post('/reset-password/:token', resetPassword);
 
 
-
-router.post('/utilisateurs', createUtilisateur);
 
 module.exports = router;
